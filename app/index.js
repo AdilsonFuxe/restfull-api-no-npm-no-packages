4f82a60505d530d8422846afec408ec88857a5a8
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-07 19:37:44 +0100

    feat: parsing http methods

--- app/index.js
+++ app/index.js
@@ -4,18 +4,20 @@
 
 // Dependencies
 const http = require('http');
-const url = require('url');
 
 // The server should respond to all request with a string
 const server = http.createServer(function (req, res) {
   //Get the path
   const path = req.url.replace(/^\/+|\/+$/g, '');
 
+  //Get the HTTP Method
+  const method = req.method.toLowerCase();
+
   //Send the Response
   res.end('Hello World\n');
 
   // Log the request path
-  console.log('Request received on path: ' + path);
+  console.log('Request received on path: ' + path + ' with method: ' + method);
 });
 
 // Start the server, and have it listen on port 3000
