fbf2d431276f7da702916b67d98e7be91cbc0de8
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-07 20:34:07 +0100

    feat: get headers and payload Request

--- app/index.js
+++ app/index.js
@@ -5,19 +5,42 @@
 // Dependencies
 const http = require('http');
 
+const StringDecoder = require('string_decoder').StringDecoder;
+
 // The server should respond to all request with a string
 const server = http.createServer(function (req, res) {
-  //Get the path
+  // Get the path
   const path = req.url.replace(/^\/+|\/+$/g, '');
 
-  //Get the HTTP Method
+  // Get the HTTP Method
   const method = req.method.toLowerCase();
 
-  //Send the Response
-  res.end('Hello World\n');
+  // Get Headers as an object
+  const headers = req.headers;
 
-  // Log the request path
-  console.log('Request received on path: ' + path + ' with method: ' + method);
+  // Get Payload Request
+  const decoder = new StringDecoder('utf8');
+  let buffer = '';
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+    // Send the Response
+    res.end('Hello World\n');
+
+    // Log the request payload
+    console.log(' Request received with this payload:', buffer);
+  });
+
+  // // Log the Request  Path
+  // console.log('Request Path: ', path);
+  // // Log the Request Query
+  // console.log('Request Query: ', {});
+  // // Log the HTTP Request Method
+  // console.log('HTTP Request Method: ', method);
+  // // Log the Headers
+  // console.log('Request Headers: ', headers);
 });
 
 // Start the server, and have it listen on port 3000
