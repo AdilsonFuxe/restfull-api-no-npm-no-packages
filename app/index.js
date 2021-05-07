037746c29bc268613343d37b9fce7e0dd551c700
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-07 19:34:35 +0100

    feat: get the path

--- app/index.js
+++ app/index.js
@@ -4,10 +4,18 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all request with a string
 const server = http.createServer(function (req, res) {
+  //Get the path
+  const path = req.url.replace(/^\/+|\/+$/g, '');
+
+  //Send the Response
   res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request received on path: ' + path);
 });
 
 // Start the server, and have it listen on port 3000
